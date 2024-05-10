e4b5e35979af737fc37919b549ef1131bf815d00
Your Name <you@example.com>
2024-05-11 00:49:45 +0100

    modification en homePage et son css

--- src/Pages/CardGallery.jsx
+++ src/Pages/CardGallery.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Cards from "./Cards";
 import { useState } from "react";
 import { cardsData } from "../data/data";
+import { motion } from "framer-motion";
 
 
 export default function CardGallery() {
@@ -19,16 +20,18 @@ export default function CardGallery() {
           {displayedCards.map((card, i) => (
             <Cards key={i} title={card.title} description={card.description} />
           ))}
+          <Link to="/survey">
+            {
+              !showAll && (
+                <motion.button className="cubutton" onClick={() => setShowAll(true)} whileHover={{ scale: 1.05 }}> Others <i class="ri-arrow-right-line" style={{marginLeft:"5px"}}></i></motion.button>
+              )
+            }
+          </Link>
         </div>
 
+
       </div>
-      <Link to="/survey">
-      {
-        !showAll && (
-          <button className="cubutton" onClick={() => setShowAll(true)}> others</button>
-        )
-      }
-      </Link>
+
 
 
 
--- src/components/Survey/Survey.jsx
+++ src/components/Survey/Survey.jsx
@@ -1,1 +1,8 @@
 
+import React from 'react'
+
+export default function Survey() {
+  return (
+    <div>Survey</div>
+  )
+}

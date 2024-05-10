2efcaeb7f5650e4b0988d19f303ad6bb70cd3eee
Amine <codingxithamine@gmail.com>
2024-05-11 00:56:15 +0100

    add survey code by amine

--- src/components/Survey/Survey.jsx
+++ src/components/Survey/Survey.jsx
@@ -1,1 +1,19 @@
+//import React from "react";
+//import "../Cards/Cards.css";
+//import { motion } from "framer-motion";
+import { cardsData } from "../../data/data";
+import Cards from "../../Pages/Cards";
 
+export default function Survey() {
+  return (
+    <div>
+      <div className="card-container">
+        <div className="card-group">
+          {cardsData.map((card, i) => (
+            <Cards key={i} title={card.title} description={card.description} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}

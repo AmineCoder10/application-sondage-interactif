abb0fad4e202000ffd2568d344a7397fc5b37726
Amine <codingxithamine@gmail.com>
2024-05-19 19:33:52 +0100

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/application-sondage-interactif/', // Replace with your repository name
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 })

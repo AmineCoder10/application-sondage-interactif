56505572846f9b472517604cad5c9f811b88dd44
Amine <codingxithamine@gmail.com>
2024-05-21 00:17:28 +0100

    modify vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,39 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/home/', // Replace with your repository name
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           vendor: ['react', 'react-dom'],
+//         },
+//       },
+//     },
+//     chunkSizeWarningLimit: 1000,
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/application-sondage-interactif/', // Replace with your repository name
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'serve' ? '/' : '/home/', // Use '/' for local dev and '/home/' for production
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+          },
         },
       },
+      chunkSizeWarningLimit: 1000,
     },
-    chunkSizeWarningLimit: 1000,
-  },
+  }
 })

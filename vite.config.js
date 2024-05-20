de6299af83720ea246569f6e3a4ba13efe2141e9
Amine <codingxithamine@gmail.com>
2024-05-21 00:01:59 +0100

    adding github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/application-sondage-interactif/', // Replace with your repository name
+  base: '/application-sondage-interactif/', // Replace with your repository name
   plugins: [react()],
   build: {
     rollupOptions: {

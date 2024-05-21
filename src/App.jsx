4e051e8c3db70c4d7dec229bb1fb4c6921f8e87b
Amine <codingxithamine@gmail.com>
2024-05-21 12:41:10 +0100

    set modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,43 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import "react-toastify/dist/ReactToastify.css";
+// import { ToastContainer } from "react-toastify";
+// import "./App.css";
+// import HomePage from "./components/HomePage/HomePage";
+// import CreateSurvey from "./components/CreateSurvey/CreateSurvey";
+// import Survey from "./components/Survey/Survey";
+// import Header from "./components/Header/Header";
+// import Footer from "./components/Footer/Footer";
+// import SurveyForm from "./components/SurveyForm/SurveyForm";
+// import OurTeam from "./components/OurTeam/OurTeam";
+// import SurveyReport from "./components/SurveyReports/SurveyReport";
+// //hashroutes | memory route
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Header />
+//       <Routes>
+//         <Route index element={<HomePage />} />
+//         <Route path="/home" element={<HomePage />} />
+//         <Route path="/create" element={<CreateSurvey />} />
+//         <Route path="/surveyForm/:id" element={<SurveyForm />} />
+//         <Route path="/SurveyReport/:surveyId" element={<SurveyReport />} />
+//         <Route path="/survey" element={<Survey />} />
+//         <Route path="/ourteam" element={<OurTeam />} />
+//       </Routes>
+//       <ToastContainer
+//         theme="dark"
+//         position="top-right"
+//         autoClose={2000}
+//         closeOnClick={false}
+//       />
+//       <Footer />
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import "./App.css";
@@ -13,7 +52,7 @@ import SurveyReport from "./components/SurveyReports/SurveyReport";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route index element={<HomePage />} />
@@ -31,7 +70,7 @@ function App() {
         closeOnClick={false}
       />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
